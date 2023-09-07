aa3badf0b301c2e3a6d4e3bfa99ad2130955a8d1
Vladimir <96481332+kitchenthinker@users.noreply.github.com>
2023-09-07 15:06:26 +0300

    Update yt_parser.py
    temporary "fix" while deploying on Cyclish

--- bot/yt_parser.py
+++ bot/yt_parser.py
@@ -94,7 +94,7 @@ class YTLiveStreamParser:
                 live = livestream_data.find("link", {"rel": "canonical"})
                 temp_livestream = YouTubeLiveStream(live.attrs['href'])
                 if temp_livestream.channel_id is not None:
-                    temp_livestream.upcoming = temp_livestream.vid_info['videoDetails'].get('isUpcoming', False)
+                    temp_livestream.upcoming = False #temp_livestream.vid_info['videoDetails'].get('isUpcoming', False)
                     temp_livestream.livestream = temp_livestream.vid_info['videoDetails'].get('isLive', False)
                     if temp_livestream.upcoming:
                         timestamp = temp_livestream.vid_info['playabilityStatus']['liveStreamability'][
